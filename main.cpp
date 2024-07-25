8ee2d00bd84b4be363a07a3a1dd64b17ff0dca0c
Georgio Zoughby <georgio.zoghby@outlook.com>
2024-07-25 14:10:36 +0300

    Added printGrid function to display sudoku board

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
-int main()
-{
-	int grid_layout[9][9];
-	std::cin.get();
+void printGrid(int* grid) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            std::cout << *(grid + i * 9 + j) << " ";
+        }
+        std::cout << std::endl;
+    }
 }
+
+int main() {
+    int grid_layout[9][9] = {0}; 
+    printGrid(&grid_layout[0][0]);
+    return 0;
+}
+
