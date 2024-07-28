72f52751c27c77d31679f499e4894175f41679b7
Georgio Zoughby <georgio.zoghby@outlook.com>
2024-07-28 11:40:32 +0300

    Testing the print grid

--- main.cpp
+++ main.cpp
@@ -10,7 +10,12 @@ void printGrid(int* grid) {
 }
 
 int main() {
-    int grid_layout[9][9] = {0}; 
+    int grid_layout[9][9] = {0};
+		for (auto &row : grid_layout) {
+			for (auto &cell : row) {
+				cell = 1;
+			}
+		}
     printGrid(&grid_layout[0][0]);
     return 0;
 }
